38ec48aa84842cf4470c2e436dc0f49bd637dbb6
TuVT <tientu115@gmail.com>
2025-05-01 11:13:37 +0700

    clear base

--- FE/src/layouts/DashboardLayout/index.jsx
+++ FE/src/layouts/DashboardLayout/index.jsx
@@ -1,15 +1,9 @@
 import PropTypes from 'prop-types';
-import Sidebar from './Sidebar';
-import TopBar from './TopBar';
 
 const DashboardLayout = ({ children }) => {
   return (
-    <div className="flex h-screen">
-      <Sidebar />
-      <div className="flex flex-col flex-1">
-        <TopBar />
-        <div className="h-full w-full">{children}</div>
-      </div>
+    <div>
+      <div className="h-full w-full">{children}</div>
     </div>
   );
 };
--- FE/src/services/tour.service.js
+++
@@ -1,6 +0,0 @@
-import mainRequest from "@/api/mainRequest";
-
-export const getTours = async (params) => {
-    const response = await mainRequest.get("/api/tours", { params });
-    return response.data;
-};
