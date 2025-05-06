d9712f88123361e661ab8cec5428eb4d229e1f13
thachnn <thach11052002@gmail.com>
2025-05-06 11:19:35 +0700

    add predefinedRole for Tour Manager

--- BE/src/main/java/com/devteria/identityservice/repository/BookingRepository.java
+++ BE/src/main/java/com/devteria/identityservice/repository/BookingRepository.java
@@ -2,6 +2,7 @@ package com.devteria.identityservice.repository;
 
 import com.devteria.identityservice.entity.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,6 +14,9 @@ public interface BookingRepository extends JpaRepository<Booking, Integer> {  //
     List<Booking> findByCustomerId(Long customerId);
 
     // Tìm kiếm tất cả các booking của một tour
+    @Query("""
+            SELECT b FROM Booking b WHERE b.tour.tourId = :tourId
+            """)
     List<Booking> findByTourId(Long tourId);
 
     // Tìm kiếm theo bookingId
