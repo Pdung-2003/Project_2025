99e51bc7fbe8658594b02f79bd989f78985610e9
thachnn <thach11052002@gmail.com>
2025-05-06 11:16:08 +0700

    config BadRequestException and ResourceNotFoundException

---
+++ BE/src/main/java/com/devteria/identityservice/exception/BadRequestException.java
@@ -0,0 +1,8 @@
+package com.devteria.identityservice.exception;
+
+public class BadRequestException extends RuntimeException{
+
+    public BadRequestException(String message){
+        super(message);
+    }
+}
---
+++ BE/src/main/java/com/devteria/identityservice/exception/ErrorResponse.java
@@ -0,0 +1,22 @@
+package com.devteria.identityservice.exception;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Builder
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponse implements Serializable {
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss", timezone = "Asia/Ho_Chi_Minh")
+    private Date timestamp;
+    private int status;
+    private String path;
+    private String error;
+    private String message;
+}
