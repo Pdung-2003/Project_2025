3aa2d9c4dfd37deab1b9c588fa39364393c13ac0
thachnn <thach11052002@gmail.com>
2025-05-06 11:19:21 +0700

    add predefinedRole for Tour Manager

--- BE/src/main/java/com/devteria/identityservice/constant/PredefinedRole.java
+++ BE/src/main/java/com/devteria/identityservice/constant/PredefinedRole.java
@@ -3,6 +3,7 @@ package com.devteria.identityservice.constant;
 public class PredefinedRole {
     public static final String USER_ROLE = "USER";
     public static final String ADMIN_ROLE = "ADMIN";
+    public static final String TOUR_MANAGER_ROLE = "TOUR_MANAGER";
 
     private PredefinedRole() {}
 }
