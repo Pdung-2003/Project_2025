436fd291cc4151d0e87aca221c27939fcbe5a979
thachnn <thach11052002@gmail.com>
2025-05-07 14:36:33 +0700

    Thêm hàm khởi tạo cho paginationResponse

--- BE/src/main/java/com/devteria/identityservice/dto/response/PaginationResponse.java
+++ BE/src/main/java/com/devteria/identityservice/dto/response/PaginationResponse.java
@@ -2,6 +2,7 @@ package com.devteria.identityservice.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
+import org.springframework.data.domain.Page;
 
 import java.io.Serializable;
 
@@ -18,4 +19,13 @@ public class PaginationResponse implements Serializable {
     private Integer totalPages;
     private Boolean hasNext;
     private Boolean isFirst;
+
+    public PaginationResponse (Page<?> page) {
+        this.page = page.getNumber();
+        this.size = page.getSize();
+        this.totalPages = page.getTotalPages();
+        this.totalElements = page.getTotalElements();
+        this.isFirst = page.isFirst();
+        this.hasNext = page.hasNext();
+    }
 }
